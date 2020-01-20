c1bbbc0408e6147e47798bb0a916d087ad787bb2
Worell97 <46696558+Worell97@users.noreply.github.com>
2020-01-20 11:59:41 -0200

    Add files via upload

---
+++ build/precache-manifest.04d80e58678d6fb2d5cb4e698e331603.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21deea91a39866272b0b1e644230de50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "979e377da182dda54f26",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a6b4a7d814fc46600a27",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "979e377da182dda54f26",
+    "url": "/static/js/2.e16fdee5.chunk.js"
+  },
+  {
+    "revision": "7f034036569bee8def7d9574fc902094",
+    "url": "/static/js/2.e16fdee5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6b4a7d814fc46600a27",
+    "url": "/static/js/main.7e0360c7.chunk.js"
+  },
+  {
+    "revision": "308963fac6b2676d80eb",
+    "url": "/static/js/runtime-main.ccc38ec6.js"
+  }
+]);
